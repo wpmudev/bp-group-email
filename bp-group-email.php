a8e8413d03224717973516daa1139777f980e096
Aaron Edwards <aaron@uglyrobot.com>
2016-12-13 16:28:35 -0600

    Update dash notice

--- bp-group-email.php
+++ bp-group-email.php
@@ -1,16 +1,16 @@
 <?php
 /*
-Plugin Name: BP Group Email
-Version: 1.0.6
-Plugin URI: http://premium.wpmudev.org/project/buddypress-group-email/
+Plugin Name: BuddyPress Group Email
+Version: 1.0.7
+Plugin URI: https://premium.wpmudev.org/project/buddypress-group-email/
 Description: This plugin adds group email functionality to BuddyPress allowing a group admin or moderator to send an email to all the other members in the group.
 Author: WPMU DEV
-Author URI: http://uglyrobot.com
+Author URI: https://premium.wpmudev.org/
 Network: true
 Textdomain: groupemail
 WDP ID: 110
 
-Copyright 2009-2014 Incsub (http://incsub.com)
+Copyright 2009-2017 Incsub (http://incsub.com)
 Author - Aaron Edwards
 Contributors - 
 
@@ -28,13 +28,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-//------------------------------------------------------------------------//
-
-//---Config---------------------------------------------------------------//
-
-//------------------------------------------------------------------------//
-
-$bp_group_email_current_version = '1.0.6';
+$bp_group_email_current_version = '1.0.7';
 
 /* Only load code that needs BuddyPress to run once BP is loaded and initialized. */
 function bp_group_email_init() {
@@ -50,15 +44,16 @@ function bp_group_email_localization() {
 }
 add_action( 'plugins_loaded', 'bp_group_email_localization' );
 
-///////////////////////////////////////////////////////////////////////////
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'install_plugins' ) )
-      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-  }
-}
-/* --------------------------------------------------------------------- */
-?>
+//load dashboard notice
+global $wpmudev_notices;
+$wpmudev_notices[] = array(
+	'id'      => 110,
+	'name'    => 'BuddyPress Group Email',
+	'screens' => array(
+		'toplevel_page_bp-groups',
+		'settings_page_bp-components',
+		'toplevel_page_bp-groups-network',
+		'settings_page_bp-components-network'
+	)
+);
+include_once( dirname( __FILE__ ) . '/dash-notice/wpmudev-dash-notification.php' );
